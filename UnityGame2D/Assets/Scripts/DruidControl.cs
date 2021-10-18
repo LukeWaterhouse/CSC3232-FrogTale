b9bd78f603bbf35a4975a296849cb8d5baf6e784
Luke <b9058369@newcastle.ac.uk>
2021-10-18 07:55:49 +0100

    working on animation

--- UnityGame2D/Assets/Scripts/DruidControl.cs
+++ UnityGame2D/Assets/Scripts/DruidControl.cs
@@ -7,21 +7,54 @@ public class DruidControl : MonoBehaviour
 
 
 {
-    [SerializeField] private float moveSpeed = 10;
-    [SerializeField] private float jumpPower = 10;
+    [SerializeField] private float moveSpeed = 8;
+    [SerializeField] private float jumpPower = 3;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
 
     private void Awake()
     {
+    
+        
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        
     }
 
     private void Update()
     {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, body.velocity.y);
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //changes velocity based on key inputs
+        body.velocity = new Vector2(horizontalInput * moveSpeed, body.velocity.y);
+        //jump
+
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = new Vector2(5,5);
+        }
+        else if (horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector2(-5,5);
+        }
+        
+        
         if (Input.GetKey(KeyCode.Space))
         {
             body.velocity = new Vector2( body.velocity.x, jumpPower);
         }
-    }
+        
+        
+        //animation
+        
+        anim.SetBool("run", horizontalInput !=0);
+}
+
+       private void Jump()
+       {
+       
+       }    
+
+
 }
