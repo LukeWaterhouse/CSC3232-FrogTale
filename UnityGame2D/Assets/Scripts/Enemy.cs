c82d0c5af5a58eb7c7654059deef556c8b58c504
b9058369 <b9058369@newcastle.ac.uk>
2021-10-21 17:10:00 +0100

    boss progress

--- UnityGame2D/Assets/Scripts/Enemy.cs
+++ UnityGame2D/Assets/Scripts/Enemy.cs
@@ -5,31 +5,57 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public bool isMoving = true;
+    Vector2 currentEulerAngles;
+    public float charFacing = 0;
+
     [SerializeField] private float moveSpeed = 8;
     private Rigidbody2D body;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("START");
-        body.velocity = new Vector2(moveSpeed, body.velocity.y);
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isMoving)
+        body.velocity = new Vector2(moveSpeed, body.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "rightPost")
         {
-            Debug.Log("isMoving");
-            Movement();
+            FlipCharacterRight();
+            
         }
-        
+
+        if (collision.gameObject.tag == "leftPost")
+        {
+            FlipCharacterLeft();
+
+        }
 
     }
 
-    void Movement()
+    private void FlipCharacterRight()
     {
-        body.velocity = new Vector2(moveSpeed * Time.fixedDeltaTime, body.velocity.y);
 
+        Debug.Log("FLIPRIGHT!"+ currentEulerAngles);
+
+        transform.eulerAngles = new Vector3(0, 180, 0);
+        //transform.localScale = new Vector2(charFacing*-1, 10);
+        moveSpeed = moveSpeed * -1;
+    }
+
+    private void FlipCharacterLeft()
+    {
+
+        Debug.Log("FLIPLEFT!" + currentEulerAngles);
+
+        transform.eulerAngles = new Vector3(0, 0, 0);
+        //transform.localScale = new Vector2(charFacing*-1, 10);
+        moveSpeed = moveSpeed * -1;
     }
 }
