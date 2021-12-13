00884b50cfd36b20e72de83ace715c30698678b0
Luke <b9058369@newcastle.ac.uk>
2021-12-13 02:36:47 +0000

    flocking working

---
+++ UnityGame2D/Assets/Scripts/FlockingScripts/Behaviour Scripts/StayInRadiusBehaviour.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Flock/Behaviour/Stay In Radius")]
+public class StayInRadiusBehaviour : FlockingBehaviour
+{
+
+
+    public Vector2 center;
+    public float radius = 15f;
+  
+    public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+       Vector2 centerOffset = (Vector2)flock.transform.position - (Vector2)agent.transform.position;
+       float t = centerOffset.magnitude / radius;
+       if (t < 0.9f){
+           return Vector2.zero;
+       }
+
+       return centerOffset * t * t;
+    }
+
+}
